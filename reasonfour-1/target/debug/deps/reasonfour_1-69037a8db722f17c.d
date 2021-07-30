29586c5c162fbcf8708b9530aac2349a1800a9d4
yan0327 <yan0327@908141467>
2021-07-30 10:32:48 +0800

    main

---
+++ reasonfour-1/target/debug/deps/reasonfour_1-69037a8db722f17c.d
@@ -0,0 +1,5 @@
+/usr/local/reasonthree/substratethree/reasonfour-1/target/debug/deps/reasonfour_1-69037a8db722f17c: src/main.rs
+
+/usr/local/reasonthree/substratethree/reasonfour-1/target/debug/deps/reasonfour_1-69037a8db722f17c.d: src/main.rs
+
+src/main.rs:
---
+++ reasonfour-1/target/debug/reasonfour-1.d
@@ -0,0 +1,1 @@
+/usr/local/reasonthree/substratethree/reasonfour-1/target/debug/reasonfour-1: /usr/local/reasonthree/substratethree/reasonfour-1/src/main.rs
